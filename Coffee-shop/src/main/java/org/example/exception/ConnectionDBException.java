78ed8d2b2f116b6325856147bd952e63549e5e4d
KochergaKostia228 <145696471+KochergaKostia228@users.noreply.github.com>
2024-12-27 14:09:45 +0200

    123

---
+++ Coffee-shop/src/main/java/org/example/exception/ConnectionDBException.java
@@ -0,0 +1,8 @@
+package org.example.exception;
+
+public class ConnectionDBException extends RuntimeException {
+    public ConnectionDBException(String message) {
+        super(message);
+    }
+}
+ 
